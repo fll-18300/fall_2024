a26cc10908900493c532df247176416a5eba5fcb
team18300 <team18300@gmail.com>
2024-12-04 12:42:55 -0500

    kyles changes

--- mission_six.py
+++ mission_six.py
@@ -16,22 +16,23 @@ def mission_six(r):
     print("Running Mission 6")
     # Mission Name
     # Authors Madeleine, Katherine
-    r.robot.straight(353)
+    r.robot.straight(328)
     r.robot.turn(13)
     wait(1000)
-    r.robot.straight(70)
+    r.robot.straight(100)
     wait(1000)
-    r.right_attachment_motor.run_time(-100,2500, then=Stop.COAST, wait=False)
+    r.right_attachment_motor.run_time(-150,3000, then=Stop.COAST, wait=False)
     # squid part
-    r.robot.turn(55)
+    r.gyro_tank_turn(200,38)
     r.robot.straight(-90)
     #CRASH
     r.robot.turn(85)
     r.robot.straight(-100)
     r.robot.straight(-400)
+    r.robot.straight(512)
     # removing the plant
-    r.robot.straight(50)
-    r.robot.turn(-55)
-    r.robot.straight(260)
-    r.right_attachment_motor.run_time(-11,2500, then=Stop.COAST, wait=False)
-    r.robot.turn(55)
+    #r.robot.straight(50)
+    #r.robot.turn(-55)
+    #r.robot.straight(260)
+    #r.right_attachment_motor.run_time(-11,2500, then=Stop.COAST, wait=False)
+    #r.robot.turn(55)
